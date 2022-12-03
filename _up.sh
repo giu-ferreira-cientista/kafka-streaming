cacb2cdb2290419588c49e99cd080f53e141e21a
giu-ferreira-cientista <giuferreira@gmail.com>
2022-12-03 20:30:07 +0000

    paths

--- _up.sh
+++ _up.sh
@@ -35,7 +35,12 @@ cd ..
 docker network create data-streaming
 docker network connect data-streaming spark
 docker network connect data-streaming pinot-controller
+docker network connect data-streaming pinot-zookeeper
+docker network connect data-streaming pinot-broker
+docker network connect data-streaming pinot-server
 docker network connect data-streaming kafka
+docker network connect data-streaming zookeeper
+docker network connect data-streaming kafka-ui
 
 
 docker exec -t pinot-controller bin/pinot-admin.sh AddTable \
